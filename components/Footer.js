b215f11df9801edbf9f4e6bdb6db6398839667a9
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-21 20:18:03 +0500

    .

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,12 @@ const Footer = () => {
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <Link href={"/"}>
               <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 px-5">
-                <Image src="/logobrand.jpg" alt="blogImg" width={150} height={ 100 } />
+                <Image
+                  src="/logobrand.jpg"
+                  alt="blogImg"
+                  width={150}
+                  height={100}
+                />
               </a>
             </Link>
             <p className="px-5 mt-2 text-sm text-gray-500">
@@ -19,7 +24,7 @@ const Footer = () => {
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              <h2 className="title-font font-medium text-red-500 tracking-widest text-sm mb-3">
                 CATEGORIES
               </h2>
               <nav className="list-none mb-10">
@@ -46,7 +51,7 @@ const Footer = () => {
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              <h2 className="title-font font-medium text-red-500 tracking-widest text-sm mb-3">
                 CATEGORIES
               </h2>
               <nav className="list-none mb-10">
@@ -73,7 +78,7 @@ const Footer = () => {
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              <h2 className="title-font font-medium text-red-500 tracking-widest text-sm mb-3">
                 CATEGORIES
               </h2>
               <nav className="list-none mb-10">
@@ -100,7 +105,7 @@ const Footer = () => {
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              <h2 className="title-font font-medium text-red-500 tracking-widest text-sm mb-3">
                 CATEGORIES
               </h2>
               <nav className="list-none mb-10">
@@ -128,21 +133,21 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="bg-gray-100">
+        <div className="bg-red-100">
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p className="text-gray-500 text-sm text-center sm:text-left">
               © 2020 Tailblocks —
               <a
                 href="https://twitter.com/knyttneve"
                 rel="noopener noreferrer"
-                className="text-gray-600 ml-1"
+                className="text-red-500 ml-1"
                 target="_blank"
               >
                 @knyttneve
               </a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <a className="text-gray-500">
+              <a className="text-red-500">
                 <svg
                   fill="currentColor"
                   stroke-linecap="round"
@@ -154,7 +159,7 @@ const Footer = () => {
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-gray-500">
+              <a className="ml-3 text-red-500">
                 <svg
                   fill="currentColor"
                   stroke-linecap="round"
@@ -166,7 +171,7 @@ const Footer = () => {
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-gray-500">
+              <a className="ml-3 text-red-500">
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -180,7 +185,7 @@ const Footer = () => {
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-gray-500">
+              <a className="ml-3 text-red-500">
                 <svg
                   fill="currentColor"
                   stroke="currentColor"

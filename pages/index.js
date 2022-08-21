a2ba2f7f18c603298fb290290215eac85701f070
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-21 20:17:50 +0500

    .

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-red-500">
               GlamorWeek
             </h1>
             <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
@@ -169,9 +168,9 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <button className="flex mx-auto mt-16 text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">
+          {/* <button className="flex mx-auto mt-16 text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">
             Button
-          </button>
+          </button> */}
         </div>
       </section>
     </div>

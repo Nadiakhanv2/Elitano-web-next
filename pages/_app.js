63159bf12739287b665a6499e27c4f4b39591397
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-22 15:34:21 +0500

    cREATING CART STATE

--- pages/_app.js
+++ pages/_app.js
@@ -22,8 +22,8 @@ function MyApp({ Component, pageProps }) {
   const saveCart = (myCart) => {
     localStorage.setItem('cart', myCart);
     let subt = 0;
-    let keys = Object.keys(cart);
-    for (let i=0; keys.length; i++ ) {
+    let keys = Object.keys(myCart);
+    for (let i=0; i < keys.length; i++ ) {
       subt += myCart[keys[i]].price * myCart[keys[i]].qty;
     }
     setSubTotal(subt);

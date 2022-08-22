633fe2bf429f9e0ab76f58908eba3c4bd48a6341
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-22 13:15:30 +0500

    pincode add

---
+++ pages/api/pincode.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json([60000 , 75850 , 46000 , 54000]);
+}

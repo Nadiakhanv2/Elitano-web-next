37367ce3a4b0f6a5bb3c7a7821e7a9c10e1ea473
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-22 14:24:04 +0500

    cREATING CART STATE

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import{useState} from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [cart, setCart] = useState({});
+  const [subTotal, setSubTotal] = useState(0);
+  const saveCart = (myCart) => {
+    localStorage.setItem('cart', myCart);
+  }
+  const addToCart = (itemCode , qty , name , price , size , variant ) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty + qty;
+    } else {
+      newCart[itemCode]= {qty : 1, price , name , size , variant}
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  }
   return (
     <>
       <Navbar />

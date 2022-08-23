8590b4661a44db035a300512517e483707d29ddc
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-23 23:00:54 +0500

    modified

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Link from "next/link";
+import {
+ 
+  AiFillPlusCircle,
+  AiFillMinusCircle,
+} from "react-icons/ai";
+import { BsFillBagCheckFill } from "react-icons/bs";
 
-const Checkout = () => {
+const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
   return (
-    <div className="container m-auto px-20">
+    <div className="container m-auto px-5 md:px-20">
       <h1 className="font-bold text-center text-3xl my-8">Checkout </h1>
       <h2 className="font-semibold text-xl">1. Delivery Details</h2>
       <div className="mx-auto flex my-2">
@@ -92,7 +99,10 @@ const Checkout = () => {
         </div>
         <div className="px-2 w-1/2">
           <div className="mb-4">
-            <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">
+            <label
+              htmlFor="pincode"
+              className="leading-7 text-sm text-gray-600"
+            >
               PinCode
             </label>
             <input
@@ -103,6 +113,58 @@ const Checkout = () => {
             />
           </div>
         </div>
+      </div>
+      <h2 className="font-semibold text-xl">1. Review Cart Items & Pay</h2>
+      <div className="sideCart  bg-red-100 p-8  m-2">
+        <ol className="list-decimal font-semibold">
+          {Object.keys(cart).length == 0 && (
+            <div className="my-4 font-semibold">Cart is Empty!</div>
+          )}
+          {Object.keys(cart).map((k) => {
+            return (
+              <li key={k}>
+                <div className="item flex my-5">
+                  <div className="">{cart[k].name}</div>
+                  <div className="flex w-1/3 justify-center items-center text-lg">
+                    <AiFillMinusCircle
+                      onClick={() => {
+                        removeFromCart(
+                          k,
+                          1,
+                          cart[k].name,
+                          cart[k].price,
+                          cart[k].variant
+                        );
+                      }}
+                      className="cursor-pointer text-red-500"
+                    />
+                    <span className="mx-1">{cart[k].qty}</span>
+                    <AiFillPlusCircle
+                      className="cursor-pointer text-red-500"
+                      onClick={() => {
+                        addToCart(
+                          k,
+                          1,
+                          cart[k].name,
+                          cart[k].price,
+                          cart[k].variant
+                        );
+                      }}
+                    />
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ol>
+        <span className="font-semibold">SubTotal : Rs{subTotal}</span>
+      </div>
+      <div className="mx-8">
+        <Link href={"/checkout"}>
+          <button className="flex mr-2 text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded">
+            <BsFillBagCheckFill className="m-1" /> Pay Rs{subTotal}
+          </button>
+        </Link>
       </div>
     </div>
   );

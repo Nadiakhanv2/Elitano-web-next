0577fc4066c30be73f8b731e0b8d3e982bfaea9f
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-25 23:04:27 +0500

    Displaying Tshirts

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,32 +1,38 @@
 import React from "react";
 import mongoose from "mongoose";
 import Product from "../models/product";
+import Link from "next/link";
 
 const Tshirts = ({ products }) => {
-  console.log(products);
   return (
     <div>
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
+        <div className="container md:px-20 py-24 mx-auto ">
           <div className="flex flex-wrap -m-4">
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full hover:shadow-lg ">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://image.made-in-china.com/2f0j00sFuEOoIGYhrf/High-Quality-Combed-Cotton-Ladies-T-Shirt.jpg"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">$16.00</p>
-              </div>
-            </div>
+            {products.map((item) => {
+              return (
+                <Link href={`/product/${item.slug}`} key={item._id} passHref={true}>
+                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full hover:shadow-lg ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <img
+                        alt="ecommerce"
+                        className="object-cover object-center w-full h-full block"
+                        src={item.img}
+                      />
+                    </a>
+                    <div className="mt-4">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        Tshirts
+                      </h3>
+                      <h2 className="text-gray-900 title-font text-lg font-medium">
+                        {item.title}
+                      </h2>
+                      <p className="mt-1">{item.price}</p>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </section>
@@ -41,7 +47,7 @@ export async function getServerSideProps(context) {
   let products = await Product.find();
 
   return {
-    props: { products : JSON.parse(JSON.stringify(products)) },
+    props: { products: JSON.parse(JSON.stringify(products)) },
   };
 }
 

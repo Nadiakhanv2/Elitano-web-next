13a7f70585e101f1a59a36203189a2b5e819c5b0
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-25 17:35:30 +0500

    Add Mongodb databse

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -1,10 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Product from '../../models/Product';
 import connectDb from '../../middleware/mongoose';
 
 const handler = async (req, res) => {
-    // await connectDb();
-    const products = await Product.find();
+     let products = await Product.find();
     res.status(200).json({products});
 }
 
